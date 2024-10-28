d9e7f7b1caf1c052b647d063fbffc010e7a3f43e
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-28 14:29:35 +0100

    add_comments

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
 /* 
 Creiamo la prima applicazione con NodeJs:
-Iniziate creando un file main.js con un semplice console.log che dica hello node js
+
 fate girare lo script usando node main.js
-aggiungete allo script una variabile di ambiente chiamata PIN  con associato numero a piacere es. PIN=666
-usate il modulo process per recuperare e stampare in console dalla proprietá env la variabile PIN
-ora passate un argomento al vostro script e recuperatene il valore usando process e la proprietá argv per stamparlo in console
-Bonus:
-se il valore dell'argomento é admin ed il valore di PIN definito é 666 allora stampa in console Welcome Admin
-altrimenti stampa Access restricted
+
+
+
 
 
 */
 
+//Iniziate creando un file main.js con un semplice console.log che dica hello node js
 
 console.log('Hello node js');
 
+//aggiungete allo script una variabile di ambiente chiamata PIN  con associato numero a piacere es. PIN=666
+
 const adminPin = 666
+
+//usate il modulo process per recuperare e stampare in console dalla proprietá env la variabile PIN
+
 const PIN = process.env.PIN
+
+//ora passate un argomento al vostro script e recuperatene il valore usando process e la proprietá argv per stamparlo in console
+
 const admin = process.argv.slice(2)
 console.log(process.env.PIN);
 console.log(process.argv);
+
+// Bonus:
+//se il valore dell'argomento é admin ed il valore di PIN definito é 666 allora stampa in console Welcome Admin
+//altrimenti stampa Access restricted
 
 if(PIN == adminPin && admin == 'admin'){
     console.log('Welcome Admin');
